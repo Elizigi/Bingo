eb515b8e2468ec82e3312050b33ac37c9c1c4015
Elizigi <elizigi876@gmail.com>
2025-06-08 22:33:37 +0300

    aa

--- Bingo/vite.config.ts
+++ Bingo/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/bingo/',
+  base: '/bingo/',
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
   plugins: [react()],
 })
